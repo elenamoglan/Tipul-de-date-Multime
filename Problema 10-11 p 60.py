98567b9394ebdcd8f9e2efc4a0c0a02026cc6924
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2021-11-10 23:43:49 +0200

    Update Problema 10-11 p 60.py

--- Problema 10-11 p 60.py
+++ Problema 10-11 p 60.py
@@ -8,19 +8,17 @@ print('Toate submulţimile mulţimii {’A’, ’B’, ’C’, ’D’}:', set
 #Problema 10
 S = {1, 2, 3, 4}
 S=list(S)
-sets = []
 len_S = len(S)
+print('Toate submulţimile mulţimii {1, 2, 3, 4}: ', end='')
 for i in range(1 << len_S):
     subset = {S[bit] for bit in range(len_S) if i & (1 << bit)}
-    sets.append(subset)
-print('Toate submulţimile mulţimii {1, 2, 3, 4}:', sets)
+    print(subset, end=',')
 
 #Problema 11
 S={'A', 'B', 'C', 'D'}
 S=list(S)
-sets = []
 len_S = len(S)
+print('\nToate submulţimile mulţimii {’A’, ’B’, ’C’, ’D’}: ', end='')
 for i in range(1 << len_S):
     subset = {S[bit] for bit in range(len_S) if i & (1 << bit)}
-    sets.append(subset)
-print('Toate submulţimile mulţimii {’A’, ’B’, ’C’, ’D’}:', sets)
+    print(subset, end=',')
